3c2a204fa4b0d2349049a0802127dcb3950fa132
Manuel José Beiras Belloso <beirasbellosomj@hotmail.com>
2024-10-05 20:26:32 +0200

    PIU Phoenix v2.02

--- NoteSkins/pump/jump/DownLeft Receptor.lua
+++
@@ -1,7 +0,0 @@
-return Def.ActorFrame {
-	LoadActor("DownLeft Tap Note")..{
-		InitCommand=cmd(stoptweening;diffusealpha,.5;visible,false);
-		ShowNoteUpcomingCommand=cmd(stoptweening;visible,true);
-		HideNoteUpcomingCommand=cmd(stoptweening;visible,false);
-	};
-}
